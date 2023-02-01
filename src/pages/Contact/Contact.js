8cf0c1a68e9da886b32bcb9ebf8e42cddf4da294
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2023-02-01 13:10:55 -0600

    fixed contact styling and functionality

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,85 +1,124 @@
-import React, { useState } from "react";
-import '../Contact/Contact.css'
-import '../../styles/structure.css'
-function Contact() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-    phone: "",
-  });
 
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
+
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import './Contact.css'
+export default function ContactUs() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+    const mailToLink = `mailto:hshafiquedev@gmail.com?subject=Contact Request&body=${encodeURIComponent(body)}`;
+    window.location.href = mailToLink;
+    setName('');
+    setEmail('');
+    setMessage('');
+
+    alert(`Your message has been sent!\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    window.location.href = `mailto:hshafiquedev@gmail.com?subject=Message from ${formData.name}&body=From: ${formData.name}%0D%0AEmail: ${formData.email}%0D%0APhone: ${formData.phone}%0D%0AMessage: ${formData.message}`;
-    setFormData({
-      name: "",
-      email: "",
-      message: "",
-      phone: "",
-    });
+
+
+
+
+  const contactConfig = {
+    YOUR_EMAIL: 'hshafiquedev@gmail.com',
+    YOUR_FONE: '(469)388-5842',
+    description:
+      'I look forward to hearing from you and our future collaborations! ',
+    YOUR_SERVICE_ID: 'service_id',
+    YOUR_TEMPLATE_ID: 'template_id',
+    YOUR_USER_ID: 'user_id',
   };
+
 
   return (
-    <div className="bg-color">
-      <div className="form-container animate__animated animate__wobble">
-        <h1 className="form-heading">Contact Me</h1>
 
+    <Container className="contain animate__animated animate__lightSpeedInLeft " >
 
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message:</label>
+      <Row className="mb-5 mt-3">
+        <Col lg="8">
+          <h1 className="display-4 mb-4">Contact Me</h1>
+          <hr className="t_border my-4 ml-0 text-left" />
+        </Col>
+      </Row>
+      <Row className="sec_sp">
+        <Col lg="5" className="mb-5">
+          <h3 className="color_sec py-4">Hadia Shafique</h3>
+          <address>
+            <strong>Email:</strong>{" "}
+            <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+              {contactConfig.YOUR_EMAIL}
+            </a>
+            <br />
+            <br />
+            {contactConfig.hasOwnProperty("YOUR_FONE") ? (
+              <p>
+                <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+              </p>
+            ) : (
+              ""
+            )}
+          </address>
+          <p>{contactConfig.description}</p>
+        </Col>
+        <Col lg="7" className="d-flex align-items-center">
+          <form onSubmit={handleSubmit} className="contact__form w-100">
+            <Row>
+              <Col lg="6" className="form-group">
+                <input
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                  required
+                />
+              </Col>
+              <Col lg="6" className="form-group">
+                <input
+                  className="form-control rounded-0"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  required
+                />
+              </Col>
+            </Row>
             <textarea
+              className="form-control rounded-0"
               id="message"
               name="message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="phone">Phone:</label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-            />
-          </div>
-          <button type="submit">Send</button>
-        </form>
+              placeholder="Message"
+              rows="5"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+              required
+            ></textarea>
+            <br />
+            <Row>
+              <Col lg="12" className="form-group">
+                <button className="btn ac_btn" type="submit">
+                  Send
+                </button>
+              </Col>
+            </Row>
+          </form>
+        </Col>
+      </Row>
+    </Container>
 
-      </div>
-    </div>
 
   );
 }
-
-export default Contact;
