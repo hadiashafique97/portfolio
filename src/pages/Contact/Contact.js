889db7836774007107ab14a8edb83410f9af8407
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2023-01-31 18:33:39 -0600

    working and styled form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,13 +1,85 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import '../Contact/Contact.css'
+import '../../styles/structure.css'
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    phone: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location.href = `mailto:hshafiquedev@gmail.com?subject=Message from ${formData.name}&body=From: ${formData.name}%0D%0AEmail: ${formData.email}%0D%0APhone: ${formData.phone}%0D%0AMessage: ${formData.message}`;
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+      phone: "",
+    });
+  };
+
   return (
-    <div className='container'>
-        <h1 className='text-center' style = {{paddingTop : "30%"}}>
-            Contact Hadia
-        </h1>
+    <div className="bg-color">
+      <div className="form-container animate__animated animate__wobble">
+        <h1 className="form-heading">Contact Me</h1>
+
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone:</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Send</button>
+        </form>
+
+      </div>
     </div>
-  )
+
+  );
 }
 
-export default Contact
+export default Contact;
